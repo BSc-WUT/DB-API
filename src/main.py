b83198eec13346154f3edab29bcd78a473b4accf
Bartolo72 <bartosz.kosinski.b@gmail.com>
2023-10-29 21:47:05 +0100

    endpoints fixes

--- src/main.py
+++ src/main.py
@@ -4,10 +4,9 @@ from elasticsearch import Elasticsearch, helpers
 from dotenv import load_dotenv
 import os
 import csv
-import json
 import io
 
-from .models import NetworkFlowMapping
+from .models import NetworkFlowMapping, NetworkFlow
 
 
 def get_env_vars() -> dict:
@@ -46,31 +45,33 @@ def network_flow_index_exists() -> None:
 """ INDEXES """
 
 
-@app.post("/indexes/create/{index_name}")
+@app.post("/indexes/create/{index_name}")  # ✅
 def create_index(
     index_name: str, network_flow_mapping: NetworkFlowMapping
 ) -> JSONResponse:
-    return es.indices.create(index=index_name, body=json.loads(network_flow_mapping))
+    return es.indices.create(index=index_name, body=network_flow_mapping.dict())
 
 
-@app.get("/indexes/{index_name}")
+@app.get("/indexes/{index_name}")  # ✅
 def index_exists(index_name: str) -> JSONResponse:
-    return es.indices.exists(index=index_name)
+    return es.indices.get(index=index_name)
 
 
 """ NETWORK FLOWS """
 
 
-@app.get("/network_flows/{flow_id}")
+@app.get("/network_flows/{flow_id}")  # ✅
 def get_network_flow(flow_id: str) -> JSONResponse:
     network_flow_index_exists()
-    query: dict = {"query": {"must": {"term": {"flow_id": flow_id}}}}
-    results: dict = es.search(index=NETWORK_FLOW_INDEX_NAME, body=query)
-    results: dict = results.get("hits", {}).get("hits", [{}])[0].get("_source")
+    try:
+        results: dict = es.get(index=NETWORK_FLOW_INDEX_NAME, id=flow_id)
+        results: dict = results.get("_source")
+    except Exception as e:
+        results: dict = {"error": str(e)}
     return results
 
 
-@app.get("/network_flows")
+@app.get("/network_flows")  # ✅
 def get_all_network_flows() -> JSONResponse:
     network_flow_index_exists()
     query: dict = {"query": {"match_all": {}}}
@@ -80,19 +81,22 @@ def get_all_network_flows() -> JSONResponse:
     return results
 
 
-@app.post("/network_flows")
-async def add_network_flow(network_flow: NetworkFlowMapping) -> JSONResponse:
+@app.post("/network_flows")  # ✅
+async def add_network_flow(network_flow: NetworkFlow) -> JSONResponse:
     network_flow_index_exists()
-    return es.index(index=NETWORK_FLOW_INDEX_NAME, doc_type="_doc", body=network_flow)
+    return es.index(
+        index=NETWORK_FLOW_INDEX_NAME, doc_type="_doc", body=network_flow.dict()
+    )
 
 
-@app.post("/network_flows/bulk_upload")
+@app.post("/network_flows/bulk_upload")  # ✅
 async def bulk_network_flow_upload(bulk_file: UploadFile) -> JSONResponse:
     network_flow_index_exists()
     contents: dict = bulk_file.file.read()
     buffer: io.StringIO = io.StringIO(contents.decode("utf-8"))
     csvReader: csv.DictReader = csv.DictReader(buffer)
     docs: list = list(csvReader)
+    print(docs)
     helpers.bulk(es, docs, index=NETWORK_FLOW_INDEX_NAME, doc_type="_doc")
     buffer.close()
     bulk_file.file.close()
